8292a44df536d376dc4f7d75b81a3f2af5e80704
newsommr <79298192+newsommr@users.noreply.github.com>
2023-12-06 18:47:02 -0500

    Add error checking

--- back-end/app/api.py
+++ back-end/app/api.py
@@ -2,22 +2,35 @@ from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
 from sqlalchemy import desc
 from database import get_db, RSSItem
+import logging
 
 router = APIRouter()
 
 @router.get("/items/")
 def read_items(limit: int = 100, db: Session = Depends(get_db)):
-    items = db.query(RSSItem)\
-        .order_by(desc(RSSItem.pubDate))\
-        .limit(limit).all()
-    return items
+    if limit <= 0:
+        raise HTTPException(status_code=400, detail="Invalid limit value. Must be > 0")
+    try:
+        items = db.query(RSSItem)\
+            .order_by(desc(RSSItem.pubDate))\
+            .limit(limit).all()
+        return items
+    except Exception as e:
+        raise HTTPException(status_code=500, detail="Internal server error")
 
 @router.get("/items/{source}")
 def read_items_by_source(source: str, limit: int = 100, db: Session = Depends(get_db)):
-    items = db.query(RSSItem)\
-              .filter(RSSItem.source == source)\
-              .order_by(desc(RSSItem.pubDate))\
-              .limit(limit).all()
-    if items is None:
-        raise HTTPException(status_code=404, detail="Items not found")
-    return items
+    if limit <= 0:
+        raise HTTPException(status_code=400, detail="Invalid limit value. Must be > 0")
+    try:
+        items = db.query(RSSItem)\
+                  .filter(RSSItem.source == source)\
+                  .order_by(desc(RSSItem.pubDate))\
+                  .limit(limit).all()
+        if not items:
+            raise HTTPException(status_code=404, detail="Items not found.")
+        return items
+    except HTTPException:
+        raise
+    except Exception as e:
+        raise HTTPException(status_code=500, detail="Internal server error")
