656e0d9c7c59368a80a6d599674184a1f08936db
newsommr <79298192+newsommr@users.noreply.github.com>
2024-06-30 17:56:18 -0400

    Rewrite API responses to conform to new standard

--- back-end/app/api.py
+++ back-end/app/api.py
@@ -1,95 +1,121 @@
-from fastapi import APIRouter, Depends, HTTPException
-from sqlalchemy import desc, func, or_
-from sqlalchemy.exc import SQLAlchemyError
 from app.models import RSSItem, SenateInfo, HouseInfo, PresidentSchedule
 from app.crud import get_db
-from datetime import datetime
+from fastapi import APIRouter, Depends, HTTPException
+from fastapi.exceptions import RequestValidationError
+from sqlalchemy import desc, func, or_
+from sqlalchemy.exc import SQLAlchemyError
 from sqlalchemy.ext.asyncio import AsyncSession
+from datetime import datetime
 import logging
+import re
 
-# Constants for readability and maintainability
-CHAMBER_SENATE = "senate"
-CHAMBER_HOUSE = "house"
-INVALID_LIMIT_DETAIL = "Invalid limit/offset value. Must be > 0"
-ITEMS_NOT_FOUND_DETAIL = "Items not found"
-INVALID_CHAMBER_DETAIL = "Invalid chamber specified"
+INVALID_BOUNDS = "Invalid limit/offset value. Must be > 0"
+ITEMS_NOT_FOUND = "Items not found"
 INTERNAL_SERVER_ERROR = "Internal server error"
+DATE_DOES_NOT_EXIST = (
+    "The supplied date format was correct, but the date is not possible."
+)
 
 router = APIRouter()
 
-def validate_pagination(limit, offset):
-    """ 
-    Validates the limit and offset for pagination
-    """
-    if limit <= 0 or offset < 0:
-        raise HTTPException(status_code=400, detail=INVALID_LIMIT_DETAIL)
 
-@router.get("/items/search/")
-async def search_items(search_term: str = "", sources: str = "", limit: int = 100, offset: int = 0, db: AsyncSession = Depends(get_db)):
+@router.get("/feed")
+async def retrieve_feed(
+    search_term: str = "",
+    sources: str = "",
+    limit: int = 100,
+    offset: int = 0,
+    db: AsyncSession = Depends(get_db),
+):
     """
-    Search RSS items by term, with optional source filtering.
+    Search entire feed, with optional filtering by sources, specific keywords, or dates.
     """
-    validate_pagination(limit, offset)
-
-    try:
+    status = "error"
+    data = None
+    message = None
+    if is_valid_bounds(limit, offset):
         rss_query = db.query(RSSItem)
         president_schedule_query = db.query(PresidentSchedule)
         potus_schedule_included = True
 
         # Apply source filtering if sources are provided
         if sources:
-            source_list = sources.split(',')
+            source_list = sources.split(",")
             rss_query = rss_query.filter(RSSItem.source.in_(source_list))
-            potus_schedule_included = 'potus-schedule' in source_list
+            potus_schedule_included = "potus-schedule" in source_list
 
         # Apply search term filtering if a search term is provided
         if search_term:
-            date_filter_applied = False
-            try:
-                # If search term is a date
-                date_obj = datetime.strptime(search_term, "%B %d, %Y")
-                rss_query = rss_query.filter(func.date(RSSItem.pubDate) == date_obj.date())
-                president_schedule_query = president_schedule_query.filter(func.date(PresidentSchedule.time) == date_obj.date())
-                date_filter_applied = True
-            except ValueError:
-                # If search term is not a date, apply text search
-                rss_query = rss_query.filter(RSSItem.title.ilike(f'%{search_term}%'))
-                if not date_filter_applied:
-                    president_schedule_query = president_schedule_query.filter(
-                        or_(
-                            PresidentSchedule.description.ilike(f'%{search_term}%'),
-                            PresidentSchedule.location.ilike(f'%{search_term}%')
-                        )
+            # If the input matches the date format, search for items with that date.
+            if is_valid_date(search_term):
+                try:
+                    date_obj = datetime.strptime(search_term, "%B %d, %Y")
+                    rss_query = rss_query.filter(
+                        func.date(RSSItem.pubDate) == date_obj.date()
                     )
+                    president_schedule_query = president_schedule_query.filter(
+                        func.date(PresidentSchedule.time) == date_obj.date()
+                    )
+                except ValueError:
+                    return formatted_response(status, data, DATE_DOES_NOT_EXIST)
+            else:
+                # If search term is not a date, apply text search
+                rss_query = rss_query.filter(RSSItem.title.ilike(f"%{search_term}%"))
+                president_schedule_query = president_schedule_query.filter(
+                    or_(
+                        PresidentSchedule.description.ilike(f"%{search_term}%"),
+                        PresidentSchedule.location.ilike(f"%{search_term}%"),
+                    )
+                )
 
-        # Fetch RSS items and POTUS schedule items without pagination
-        rss_items = rss_query.order_by(desc(RSSItem.pubDate)).all()
+        # Fetch RSS items and POTUS schedule items with pagination
+        rss_items = (
+            rss_query.order_by(desc(RSSItem.pubDate)).offset(offset).limit(limit).all()
+        )
+        rss_items = [
+            {
+                "title": item.title,
+                "link": item.link,
+                "pubDate": item.time,
+                "source": item.source,
+                "fetched_at": item.fetched_at,
+            }
+            for item in rss_items
+        ]
+
         president_schedule_items = []
         if potus_schedule_included:
-            president_schedule_items = president_schedule_query.all()
-            president_schedule_items = [format_president_schedule_item(item) for item in president_schedule_items]
+            president_schedule_items = (
+                president_schedule_query.offset(offset).limit(limit).all()
+            )
+            president_schedule_items = [
+                format_president_schedule_item(item)
+                for item in president_schedule_items
+            ]
 
-        # Combine, sort, and then apply pagination to the combined list
-        combined_items = sorted(rss_items + president_schedule_items, key=get_pub_date, reverse=True)
-        paginated_combined_items = combined_items[offset:offset + limit]
+        # Combine, sort, and then return the paginated results
+        combined_items = sorted(
+            rss_items + president_schedule_items, key=get_pub_date, reverse=True
+        )
 
-        # Handle case when no items are found
-        if not paginated_combined_items and offset == 0:
-            raise HTTPException(status_code=404, detail=ITEMS_NOT_FOUND_DETAIL)
-
-        return paginated_combined_items
-    except Exception as e:
-        logging.error(f"Error in search items: {e}")
-        raise HTTPException(status_code=500, detail=INTERNAL_SERVER_ERROR)
+        if combined_items:
+            status = "success"
+            data = combined_items
+        else:
+            message = ITEMS_NOT_FOUND
+    else:
+        message = INVALID_BOUNDS
+    return formatted_response(status, data, message)
 
 
 def get_pub_date(item):
     if isinstance(item, RSSItem):
         return item.pubDate
     elif isinstance(item, dict):
-        return item['pubDate']
+        return item["pubDate"]
     else:
-        return None  # or some default date
+        return None
+
 
 def format_president_schedule_item(item):
     """
@@ -103,64 +129,102 @@ def format_president_schedule_item(item):
 
     # Format item as RSSItem-like dictionary
     formatted_item = {
-        'title': title,
-        'link': item.link,
-        'pubDate': item.time,
-        'source': 'potus-schedule'
+        "title": title,
+        "link": item.link,
+        "pubDate": item.time,
+        "source": "potus-schedule",
     }
     return formatted_item
 
 
-
-@router.get("/info/session/{chamber}")
-async def get_congress_session_info(chamber: str, db: AsyncSession = Depends(get_db)):
+@router.get("/legislative/session-info")
+async def get_congress_session_info(db: AsyncSession = Depends(get_db)):
     """
-    Get next meeting information for a specific congressional chamber.
+    Returns the next meeting information for the House/Senate.
     """
-    try:
-        if chamber == CHAMBER_SENATE:
-            result = db.query(SenateInfo).first()
-        elif chamber == CHAMBER_HOUSE:
-            result = db.query(HouseInfo).first()
-        else:
-            raise HTTPException(status_code=400, detail=INVALID_CHAMBER_DETAIL)
+    status = "error"
+    data = None
+    message = None
 
-        if result is None:
-            raise HTTPException(status_code=404, detail=ITEMS_NOT_FOUND_DETAIL)
+    senate = db.query(SenateInfo).first()
+    house = db.query(HouseInfo).first()
 
-        return {
-            "in_session": result.in_session,
-            "next_meeting": result.next_meeting,
-            "live_link": result.live_link,
-            "last_updated": result.last_updated,
-        }
-    except Exception as e:
-        logging.error(f"Error fetching congress session info for {chamber}: {e}")
-        raise HTTPException(status_code=500, detail=INTERNAL_SERVER_ERROR)
+    if senate and house:
+        status = "success"
+        data = [
+            {
+                "chamber": "senate",
+                "in_session": senate.in_session,
+                "next_meeting": senate.next_meeting,
+                "live_link": senate.live_link,
+                "last_updated": senate.last_updated,
+            },
+            {
+                "chamber": "house",
+                "in_session": house.in_session,
+                "next_meeting": house.next_meeting,
+                "live_link": house.live_link,
+                "last_updated": house.last_updated,
+            },
+        ]
+    else:
+        message = ITEMS_NOT_FOUND
 
-@router.get("/info/president-schedule/")
-async def get_president_session_info(limit: int = 100, offset: int = 0, db: AsyncSession = Depends(get_db)):
+    return formatted_response(status, data, message)
+
+
+@router.get("/executive/potus-schedule/")
+async def get_potus_schedule(
+    limit: int = 100, offset: int = 0, db: AsyncSession = Depends(get_db)
+):
     """
     Returns the President's public schedule, with optional source filtering and pagination.
     """
-    validate_pagination(limit, offset)
-    try:
-        query = db.query(PresidentSchedule).order_by(desc(PresidentSchedule.time)).offset(offset).limit(limit)
+    status = "error"
+    data = None
+    message = None
+    result = None
+    if is_valid_bounds(limit, offset):
+        result = (
+            db.query(PresidentSchedule)
+            .order_by(desc(PresidentSchedule.time))
+            .offset(offset)
+            .limit(limit)
+            .all()
+        )
+    else:
+        message = INVALID_BOUNDS
 
-        # Execute the query
-        result = query.all()
+    if result:
+        status = "success"
+        data = [
+            {
+                "description": item.description,
+                "link": item.link,
+                "location": item.location,
+                "time": item.time,
+                "pubDate": item.time,
+                "press_information": item.press_information,
+                "last_updated": item.last_updated,
+            }
+            for item in result
+        ]
+    else:
+        message = ITEMS_NOT_FOUND
+    return formatted_response(status, data, message)
 
-        if result is None:
-            raise HTTPException(status_code=404, detail=ITEMS_NOT_FOUND_DETAIL)
 
-        return result
-    except Exception as e:
-        logging.error(f"Error fetching the President's Schedule: {e}")
-        raise HTTPException(status_code=500, detail=INTERNAL_SERVER_ERROR)
+def formatted_response(status, data, message):
+    return {"status": status, "data": data, "message": message}
 
-def format_response(status, data, message):
-    return {
-        "status": status,
-        "data": data,
-        "message": message
-    }
+
+def is_valid_bounds(limit, offset):
+    """
+    Validates the limit and offset for pagination
+    """
+    return limit > 0 and offset >= 0
+
+
+def is_valid_date(search_term):
+    pattern = r"^(January|February|March|April|May|June|July|August|September|October|November|December) \d{1,2}, \d{4}$"
+    return re.match(pattern, search_term)
