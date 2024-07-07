9305b849bcb163401264b11f5da5fd30e0595e73
newsommr <79298192+newsommr@users.noreply.github.com>
2024-07-06 23:48:26 -0400

    Update crud.py

--- back-end/app/crud.py
+++ back-end/app/crud.py
@@ -11,34 +11,49 @@ from app.time_util import current_time
 SENATE_SOURCE = "senateppg-twitter"
 HOUSE_SOURCE = "housedailypress-twitter"
 
+
 def get_db():
     with SessionLocal() as db:
         yield db
 
+
 def add_feed_item(db: Session, feed_item: dict):
-    
-    title, pubDate, link = feed_item['title'], feed_item['pubDate'], feed_item['link']
-    existing_item = db.query(FeedItem).filter_by(title=title, pubDate=pubDate, link=link).first()
+
+    title, pubDate, link = feed_item["title"], feed_item["pubDate"], feed_item["link"]
+    existing_item = (
+        db.query(FeedItem).filter_by(title=title, pubDate=pubDate, link=link).first()
+    )
     if existing_item:
         return
     new_item = FeedItem(**feed_item)
     db.add(new_item)
 
 
-def update_meeting_info(db: Session, chamber: str, in_session: int, next_meeting = None, live_link: str = None):
+def update_meeting_info(
+    db: Session, chamber: str, in_session: int, next_meeting=None, live_link: str = None
+):
 
     session_info = db.query(SessionInfo).filter_by(chamber=chamber).delete()
     new_item = SessionInfo(
-        chamber = chamber,
-        meeting_date = next_meeting,
-        in_session = in_session,
-        live_link = live_link,
+        chamber=chamber,
+        meeting_date=next_meeting,
+        in_session=in_session,
+        live_link=live_link,
     )
     db.add(new_item)
 
+
 def update_president_schedule(db: Session, president_schedule: dict):
-    time, location, description = president_schedule['time'], president_schedule['location'], president_schedule['description']
-    existing_item = db.query(PresidentSchedule).filter_by(time=time, location=location, description=description).first()
+    time, location, description = (
+        president_schedule["time"],
+        president_schedule["location"],
+        president_schedule["description"],
+    )
+    existing_item = (
+        db.query(PresidentSchedule)
+        .filter_by(time=time, location=location, description=description)
+        .first()
+    )
     if existing_item:
         return
 
